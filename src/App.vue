099235e8fffdf6930f299ccd5e9571d1054c1e5f
Mickaël  Anicette <mickaelanicette@gmail.com>
2022-12-09 15:12:31 -0300

    Improve use quizzs, questions and result component on the main app
    * Use all components on the main app
    * Improve the css to match with all component
    * Add cypress folder
    * Add end to end test for the app

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,241 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
+  <header></header>
+  <quizzs
+    v-if="!selectedQuizz"
+    :quizzs="quizzs"
+    @select-a-quizz="selectAQuizz"
+  />
+  <div v-else class="ctr">
+    <questions
+      v-if="questionsAnswered < selectedQuizz.questions.length"
+      :questions="selectedQuizz.questions"
+      :questionsAnswered="questionsAnswered"
+      @question-answered="updateScoreAndQuizz"
     />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <result v-else :results="selectedQuizz.results" :score="score" />
+    <div v-if="questionsAnswered >= selectedQuizz.questions.length">
+      <button type="button" class="redo-btn" @click.prevent="redoQuizz">
+        redo
+      </button>
+      <button
+        type="button"
+        class="change-quizz-btn"
+        @click.prevent="changeQuizz"
+      >
+        change quizz
+      </button>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import Questions from "./components/Questions.vue";
+import Result from "./components/Result.vue";
+import Quizzs from "./components/Quizzs.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  name: "App",
+  components: {
+    Questions,
+    Result,
+    Quizzs,
+  },
+  data() {
+    return {
+      selectedQuizz: null,
+      questionsAnswered: 0,
+      score: 0,
+      quizzs: [
+        {
+          id: 1,
+          title: "math",
+          questions: [
+            {
+              id: 1,
+              question: "What is 5 + 5 ?",
+              answers: [
+                {
+                  text: "10",
+                  is_correct: true,
+                },
+                {
+                  text: "5",
+                  is_correct: false,
+                },
+                {
+                  text: "25",
+                  is_correct: false,
+                },
+                {
+                  text: "15",
+                  is_correct: false,
+                },
+              ],
+            },
+            {
+              id: 2,
+              question: "What is 5 * 5 ?",
+              answers: [
+                {
+                  text: "50",
+                  is_correct: false,
+                },
+                {
+                  text: "55",
+                  is_correct: false,
+                },
+                {
+                  text: "25",
+                  is_correct: true,
+                },
+                {
+                  text: "10",
+                  is_correct: false,
+                },
+              ],
+            },
+            {
+              id: 3,
+              question: "What is 5 / 5 ?",
+              answers: [
+                {
+                  text: "0",
+                  is_correct: false,
+                },
+                {
+                  text: "1",
+                  is_correct: true,
+                },
+                {
+                  text: "5",
+                  is_correct: false,
+                },
+                {
+                  text: "15",
+                  is_correct: false,
+                },
+              ],
+            },
+          ],
+          results: [
+            {
+              min: 0,
+              max: 2,
+              title: "Try again!",
+              desc: "Do a little more studying and you may succeed!",
+            },
+            {
+              min: 3,
+              max: 3,
+              title: "Wow, you're a genius!",
+              desc: "Studying has definitely paid off for you!",
+            },
+          ],
+        },
+        {
+          id: 2,
+          title: "default",
+          questions: [
+            {
+              id: 1,
+              question: "What is 2 + 2?",
+              answers: [
+                {
+                  text: "4",
+                  is_correct: true,
+                },
+                {
+                  text: "3",
+                  is_correct: false,
+                },
+                {
+                  text: "8",
+                  is_correct: false,
+                },
+                {
+                  text: "5",
+                  is_correct: false,
+                },
+              ],
+            },
+            {
+              id: 2,
+              question: 'How many letters are in the word "Banana"?',
+              answers: [
+                {
+                  text: "5",
+                  is_correct: false,
+                },
+                {
+                  text: "7",
+                  is_correct: false,
+                },
+                {
+                  text: "6",
+                  is_correct: true,
+                },
+                {
+                  text: "12",
+                  is_correct: false,
+                },
+              ],
+            },
+            {
+              id: 3,
+              question: "Find the missing letter: C_ke",
+              answers: [
+                {
+                  text: "e",
+                  is_correct: false,
+                },
+                {
+                  text: "a",
+                  is_correct: true,
+                },
+                {
+                  text: "i",
+                  is_correct: false,
+                },
+              ],
+            },
+          ],
+          results: [
+            {
+              min: 0,
+              max: 2,
+              title: "Try again!",
+              desc: "Do a little more studying and you may succeed!",
+            },
+            {
+              min: 3,
+              max: 3,
+              title: "Wow, you're a genius!",
+              desc: "Studying has definitely paid off for you!",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    selectAQuizz(quizzIndex) {
+      this.selectedQuizz = this.quizzs.find((quizz) => quizz.id === quizzIndex);
+    },
+    updateScoreAndQuizz(answerResult) {
+      answerResult ? this.score++ : null;
+      this.questionsAnswered++;
+    },
+    redoQuizz() {
+      this.questionsAnswered = 0;
+      this.score = 0;
+    },
+    changeQuizz() {
+      this.selectedQuizz = null;
+      this.questionsAnswered = 0;
+      this.score = 0;
+    },
+  },
+};
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
